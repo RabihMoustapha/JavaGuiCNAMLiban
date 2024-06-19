6d475b9eb10e137a705c5acec30d8e0eb5ff8a01
RabihMoustapha <mostapharabih59@gmail.com>
2024-06-19 12:27:49 +0300

    Add new data to the project with a new classes ;)

--- testMcd.java
+++ testMcd.java
@@ -8,6 +8,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 
+
 public class testMcd extends JFrame {
 
     private JTabbedPane demo;
@@ -36,15 +37,29 @@ public class testMcd extends JFrame {
     private JScrollPane scrollPane1;
     private JComboBox<String> rA1, prA;
 
+    // Tache
+    // Filter
+    private JLabel filterLabel1;
+    private TableRowSorter<DefaultTableModel> sorter1;
+    private JTextField filterText1;
+    private JButton filterButton1;
+
+    private JPanel p9, p10, p11, p12;
+    private JLabel idL2, cL2, dL2, eL2;
+    private JTextField id2, c2, d2, e2;
+    private JButton reload2, reset2, add2, addProcessus;
+    private JTable table2;
+    private JScrollPane scrollPane2;
+
     public testMcd() {
-        // Mcd Processus
+        //Processus
         demo = new JTabbedPane();
         p1 = new JPanel(new GridLayout(7, 2));
         p2 = new JPanel();
         p3 = new JPanel();
         p4 = new JPanel(new BorderLayout());
 
-        // Processus Labels
+        //Labels
         idL = new JLabel("Identité");
         raL = new JLabel("Resource Affectée");
         dL = new JLabel("Durée");
@@ -52,7 +67,7 @@ public class testMcd extends JFrame {
         eL = new JLabel("État");
         filterLabel = new JLabel("Filter");
 
-        // Processus TextFields
+        //TextFields
         id = new JTextField();
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();
         rA = new JComboBox<String>(model);
@@ -72,7 +87,7 @@ public class testMcd extends JFrame {
         filterButton = new JButton("Filter");
 
         // Processus JTable componets
-        String[] columnNames = {"Identité", "Resource Affectée", "Cout", "Durée", "État"};
+        String[] columnNames = {"Identité", "Resource Affectée", "Cout", "Durée", "État", "Total"};
         DefaultTableModel model2 = new DefaultTableModel(columnNames, 0);
         table = new JTable(model2);
         scrollPane = new JScrollPane(table);
@@ -193,7 +208,7 @@ public class testMcd extends JFrame {
                     new ResourceHumaine();
                 }
 
-                if(selected.equals("Resource Materielle")){
+                if (selected.equals("Resource Materielle")) {
                     new ResourceMaterielle();
                 }
             }
@@ -316,6 +331,77 @@ public class testMcd extends JFrame {
                 model2.setRowCount(0);
             }
         });
+
+        //Tache
+        p9 = new JPanel(new GridLayout(6, 2));
+        p10 = new JPanel();
+        p11 = new JPanel();
+        p12 = new JPanel(new BorderLayout());
+
+        //Labels
+        filterLabel1 = new JLabel("Filter");
+        idL2 = new JLabel("Identité");
+        cL2 = new JLabel("Cout");
+        dL2 = new JLabel("Durée");
+        eL2 = new JLabel("Etat");
+
+        //TextFields
+        filterText1 = new JTextField();
+        id2 = new JTextField();
+        e2 = new JTextField();
+        d2 = new JTextField();
+        c2 = new JTextField();
+
+        //Buttons
+        reset2 = new JButton("Reset");
+        add2 = new JButton("Add");
+        reload2 = new JButton("Reload");
+        filterButton1 = new JButton("Filter");
+        addProcessus = new JButton("Add Processus");
+        JButton ReadProcessusButton = new JButton("Read processus data");
+
+        //Add componets
+        p9.add(idL2);
+        p9.add(id2);
+
+        p9.add(cL2);
+        p9.add(c2);
+
+        p9.add(eL2);
+        p9.add(e2);
+
+        p9.add(dL2);
+        p9.add(d2);
+
+        p9.add(filterLabel1);
+        p9.add(filterText1);
+
+        p10.add(add2);
+        p10.add(reset2);
+        p10.add(reload2);
+        p10.add(filterButton1);
+        p10.add(addProcessus);
+        p10.add(ReadProcessusButton);
+
+        //Add panels to the main panel
+        p12.add(p9, BorderLayout.NORTH);
+        p12.add(p10, BorderLayout.CENTER);
+        //p12.add(p11, BorderLayout.SOUTH);
+
+        demo.add("Tache", p12);
+
+        addProcessus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Processus p = new Processus();
+            }
+        });
+
+        ReadProcessusButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new readProcessus();
+            }
+        });
+
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
@@ -327,6 +413,7 @@ public class testMcd extends JFrame {
 }
 
 class ResourceHumaine extends JFrame {
+
     // Filter
     private JLabel filterLabel;
     private TableRowSorter<DefaultTableModel> sorter;
@@ -343,7 +430,7 @@ class ResourceHumaine extends JFrame {
 
     public ResourceHumaine() {
         super("Resurce huamine");
-        
+
         p1 = new JPanel(new GridLayout(5, 2));
         p2 = new JPanel();
         p3 = new JPanel();
@@ -438,8 +525,9 @@ class ResourceHumaine extends JFrame {
                 sorter.setRowFilter(rf);
                 table.setRowSorter(sorter);
             }
-    });
-    setVisible(true);
+        });
+        setVisible(true);
+        setSize(300, 300);
     }
 }
 
@@ -551,5 +639,190 @@ class ResourceMaterielle extends JFrame {
             }
         });
         setVisible(true);
+        setSize(300, 300);
+    }
+}
+
+class Processus extends JFrame {
+    // Filter
+
+    private JLabel filterLabel;
+    private TableRowSorter<DefaultTableModel> sorter;
+    private JTextField filterText;
+    private JButton filterButton;
+
+    // Processus 
+    private JPanel p1, p2, p3, p4; // Panels
+    private JLabel idL, raL, cL, dL, eL; // Labels
+    private JTextField id, c, d, e; // TextFields
+    private JButton reload, reset, add, read;
+    private JTable table;
+    private JComboBox<String> rA;
+    private JScrollPane scrollPane;
+
+    public Processus() {
+        super("Processus");
+        p1 = new JPanel(new GridLayout(7, 2));
+        p2 = new JPanel();
+        p3 = new JPanel();
+        p4 = new JPanel(new BorderLayout());
+
+        //Labels
+        idL = new JLabel("Identité");
+        raL = new JLabel("Resource Affectée");
+        dL = new JLabel("Durée");
+        cL = new JLabel("Cout");
+        eL = new JLabel("État");
+        filterLabel = new JLabel("Filter");
+
+        //TextFields
+        id = new JTextField();
+        DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();
+        rA = new JComboBox<String>(model);
+        rA.addItem("");
+        rA.addItem("Resource Humaine");
+        rA.addItem("Resource Materielle");
+        JScrollPane rAs = new JScrollPane(rA);
+        c = new JTextField();
+        d = new JTextField();
+        e = new JTextField();
+
+        // Processus Buttons
+        reload = new JButton("Reload");
+        reset = new JButton("Reset");
+        add = new JButton("Add");
+        read = new JButton("ReadData");
+        filterButton = new JButton("Filter");
+
+        // Processus JTable componets
+        String[] columnNames = {"Identité", "Resource Affectée", "Cout", "Durée", "État"};
+        DefaultTableModel model2 = new DefaultTableModel(columnNames, 0);
+        table = new JTable(model2);
+        scrollPane = new JScrollPane(table);
+
+        // Filter componets
+        sorter = new TableRowSorter<>(model2);
+        table.setRowSorter(sorter);
+        filterText = new JTextField();
+        filterText.setToolTipText("Filter");
+
+        p1.add(idL);
+        p1.add(id);
+
+        p1.add(raL);
+        p1.add(rAs);
+
+        p1.add(cL);
+        p1.add(c);
+
+        p1.add(eL);
+        p1.add(e);
+
+        p1.add(dL);
+        p1.add(d);
+        p1.add(filterLabel);
+        p1.add(filterText);
+
+        p2.add(add);
+        p2.add(reload);
+        p2.add(reset);
+        p2.add(read);
+        p2.add(filterButton);
+
+        p3.add(scrollPane);
+        p4.add(p1, BorderLayout.NORTH);
+        p4.add(p2, BorderLayout.CENTER);
+        p4.add(p3, BorderLayout.SOUTH);
+        add(p4);
+        // Reset
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e1) {
+                // Clear the text fields
+                c.setText("");
+                d.setText("");
+                id.setText("");
+                e.setText("");
+            }
+        });
+
+        // Reload
+        reload.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Clear all rows from the table
+                model2.setRowCount(0);
+            }
+        });
+
+        // Add
+        add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e2) {
+                String id1 = id.getText();
+                String c1 = c.getText();
+                String e1 = e.getText();
+                String d1 = d.getText();
+
+                if (id1 != null && c1 != null && e1 != null && d1 != null) {
+                    try {
+                        FileWriter w = new FileWriter("Processus.txt");
+                        BufferedWriter bw = new BufferedWriter(w);
+                        bw.write(id1 + ", " + rA.getSelectedItem() + ", " + c1 + ", " + e1 + ", " + d1 + "\n");
+                        bw.newLine();
+                        JOptionPane.showMessageDialog(null, "DataAdded");
+                    } catch (IOException ioe) {
+                        JOptionPane.showMessageDialog(null, "Error");
+                    }
+                }
+            }
+        });
+
+        // Read data
+        read.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    FileReader reader = new FileReader("Processus.txt");
+                    BufferedReader br = new BufferedReader(reader);
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        if(!line.isEmpty()){
+                        String[] elt = line.split(",");
+                        model2.addRow(elt);
+                        }
+                    }
+                } catch (IOException ioe) {
+                    JOptionPane.showMessageDialog(null, "Error");
+                }
+
+            }
+        });
+        //Filter action listner
+        filterButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String filter = filterText.getText();
+                sorter = new TableRowSorter<>(model2);
+                RowFilter<DefaultTableModel, Object> rf = null;
+                rf = RowFilter.regexFilter(filter);
+                sorter.setRowFilter(rf);
+                table.setRowSorter(sorter);
+            }
+        });
+
+        //comboBox action
+        rA.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String selected = (String) rA.getSelectedItem();
+                if (selected.equals("Resource Humaine")) {
+                    new ResourceHumaine();
+                }
+
+                if (selected.equals("Resource Materielle")) {
+                    new ResourceMaterielle();
+                }
+            }
+        });
+        setVisible(true);
+        setSize(300, 300);
     }
 }
